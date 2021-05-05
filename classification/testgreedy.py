edea889dcc8ca2b347a09fa40a80c40434a3a180
Yangyeeee <42969395+Yangyeeee@users.noreply.github.com>
2021-05-04 22:48:47 -0400

    Update testgreedy.py

--- classification/testgreedy.py
+++ classification/testgreedy.py
@@ -9,12 +9,12 @@ from tqdm import tqdm
 import sys
 import importlib
 import time
+import provider
 from time import localtime
 from torch.utils.tensorboard import SummaryWriter
 from data.modelnet_loader_torch import ModelNetCls
-import torch.nn.functional as F
-
 import torchvision
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = BASE_DIR
 sys.path.append(os.path.join(ROOT_DIR, 'models'))
@@ -23,6 +23,7 @@ sys.path.append(os.path.join(ROOT_DIR, 'models'))
 def parse_args():
     '''PARAMETERS'''
     parser = argparse.ArgumentParser('SSN')
+    parser.add_argument('-b','--batch_size', type=int, default=512, help='batch size in training [default: 32]')
     parser.add_argument('--model', default='pointnet_cls', help='model name [default: pointnet_cls]')
     parser.add_argument('--gpu', type=str, default='0', help='specify gpu device [default: 0]')
     parser.add_argument('--log_dir', type=str, default='pointnet', help='experiment root')
@@ -30,7 +31,8 @@ def parse_args():
     parser.add_argument('--normal', action='store_true', default=False, help='Whether to use normal information [default: False]')
     parser.add_argument('--datafolder',  type=str, help='dataset folder')
     parser.add_argument("-in", "--num-in-points", type=int, default=1024, help="Number of input Points [default: 1024]")
-
+    parser.add_argument('--beta', default=1.0, type=float, help='beta for coverage loss')
+    parser.add_argument('--max', action='store_true', default=False, help='using FPS')
     # For testing
     parser.add_argument('--test', action='store_true', help='Perform testing routine. Otherwise, the script will train.')
     return parser.parse_args()
@@ -81,24 +83,108 @@ def get_datasets(args):
 
     return trainset, testset
 
-def test_greedy(model, loader, writer, num_class=40):
+def square_distance(src, dst):
+    """
+    Calculate Euclid distance between each two points.
 
-    index = (torch.ones((2468,1024)) * (torch.tensor([i for i in range(1024)]).view(-1,1024))).long().cuda()
+    src^T * dst = xn * xm + yn * ym + zn * zm;
+    sum(src^2, dim=-1) = xn*xn + yn*yn + zn*zn;
+    sum(dst^2, dim=-1) = xm*xm + ym*ym + zm*zm;
+    dist = (xn - xm)^2 + (yn - ym)^2 + (zn - zm)^2
+         = sum(src**2,dim=-1)+sum(dst**2,dim=-1)-2*src^T*dst
+
+    Input:
+        src: source points, [B, N, C]
+        dst: target points, [B, M, C]
+    Output:
+        dist: per-point square distance, [B, N, M]
+    """
+    B, _, N = src.shape
+    _, _, M = dst.shape
+    dist = -2 * torch.matmul(src.permute(0, 2, 1), dst)
+    dist += torch.sum(src ** 2, 1).view(B, N, 1)
+    dist += torch.sum(dst ** 2, 1).view(B, 1, M)
+    return dist
+
+
+# def test_greedy(model, loader, writer, num_class=40):
+#     index = (torch.ones((2468,1024)) * (torch.tensor([i for i in range(1024)]).view(-1,1024))).long().cuda()
+#     unselected = torch.ones((2468, 1024)).bool().cuda()
+#     selected = torch.zeros((2468, 1024)).bool().cuda()
+#     for j, data in tqdm(enumerate(loader), total=len(loader)):
+#         points, target = data
+#         target = target[:, 0]
+#         points, target = points.cuda(), target.cuda()
+#         selected_ind = index[selected].reshape(2468, -1)
+#         for i in range(32):
+#             class_acc = np.zeros((num_class, 3))
+#             selected_point = batched_index_select(points, 1, selected_ind)
+#             unselected_ind = index[unselected].reshape(2468, -1)
+#             logit = []
+#
+#             for k in range(1024 - i):
+#                 tmp_index = unselected_ind[:,k].reshape(2468,-1)
+#                 tmp = batched_index_select(points, 1, tmp_index)
+#                 if selected_point.shape[1] != 0:
+#                     simplified = torch.cat((selected_point, tmp),dim=1)
+#                 else:
+#                     simplified = tmp
+#                 simplified = simplified.transpose(2, 1)
+#                 classifier = model.eval()
+#                 pred, trans_feat = classifier(simplified)
+#                 #tmp_logit = F.softmax(pred,dim=-1).max(-1)[0]
+#                 tmp_logit = pred.max(-1)[0]
+#                 logit.append(tmp_logit.reshape(-1,1))
+#
+#             sel = torch.gather(unselected_ind,dim=-1, index=torch.argmax(torch.cat(logit,dim=-1),dim=-1).reshape(-1,1))
+#             #sel = torch.gather(unselected_ind, dim=-1, index=torch.LongTensor(2468, 1).random_(0, 1024-i).cuda())
+#             selected_ind = torch.cat((selected_ind,sel),dim=-1)
+#             unselected = torch.ones((2468, 1024),dtype=bool).cuda().scatter_(dim=-1,index=selected_ind,src=torch.tensor(0,dtype=bool))
+#
+#             simplified = batched_index_select(points, 1, selected_ind)
+#             classifier = model.eval()
+#             simplified = simplified.transpose(2, 1)
+#             pred, trans_feat = classifier(simplified)
+#             pred_choice = pred.data.max(1)[1]
+#             for cat in np.unique(target.cpu()):
+#                 classacc = pred_choice[target==cat].eq(target[target==cat].long().data).cpu().sum()
+#                 class_acc[cat,0]+= classacc.item()/float(points[target==cat].size()[0])
+#                 class_acc[cat,1]+=1
+#             correct = pred_choice.eq(target.long().data).cpu().sum()
+#             class_acc[:,2] =  class_acc[:,0] / class_acc[:,1]
+#             class_acc = np.mean(class_acc[:,2])
+#             instance_acc = correct.item() / points.size()[0]
+#
+#             print('%f  Test Instance Accuracy: %f, Class Accuracy: %f' % ( i, instance_acc, class_acc))
+#             writer.add_scalar('acc/test_i', instance_acc,i )
+#             writer.add_scalar('acc/test_c', class_acc, i)
+
+def test_greedy_batch(model, loader, writer, num_class=40):
+
     unselected = torch.ones((2468, 1024)).bool().cuda()
     selected = torch.zeros((2468, 1024)).bool().cuda()
-    for j, data in tqdm(enumerate(loader), total=len(loader)):
-        points, target = data
-        target = target[:, 0]
-        points, target = points.cuda(), target.cuda()
-        selected_ind = index[selected].reshape(2468, -1)
-        for i in range(32):
-            class_acc = np.zeros((num_class, 3))
-            selected_point = batched_index_select(points, 1, selected_ind)
-            unselected_ind = index[unselected].reshape(2468, -1)
+    index = (torch.ones((2468, 1024)) * (torch.tensor([i for i in range(1024)]).view(-1, 1024))).long().cuda()
+    for i in range(32):
+        class_acc = np.zeros((num_class, 3))
+        p = []
+        t = []
+        for j, data in tqdm(enumerate(loader), total=len(loader)):
+            points, target = data
+            target = target[:, 0]
+            N = points.shape[0]
+            points, target = points.cuda(), target.cuda()
+            p.append(points)
+            t.append(target)
+            ind_l = j*args.batch_size
+            ind_h = min((j+1)*args.batch_size,2468)
+            selected_batch = selected[ind_l:ind_h]
+            unselected_batch = unselected[ind_l:ind_h]
+            selected_ind_batch = index[ind_l:ind_h][selected_batch].reshape(N, -1)
+            unselected_ind_batch = index[ind_l:ind_h][unselected_batch].reshape(N, -1)
+            selected_point = batched_index_select(points, 1, selected_ind_batch)
             logit = []
-
             for k in range(1024 - i):
-                tmp_index = unselected_ind[:,k].reshape(2468,-1)
+                tmp_index = unselected_ind_batch[:,k].reshape(N,-1)
                 tmp = batched_index_select(points, 1, tmp_index)
                 if selected_point.shape[1] != 0:
                     simplified = torch.cat((selected_point, tmp),dim=1)
@@ -108,31 +194,44 @@ def test_greedy(model, loader, writer, num_class=40):
                 classifier = model.eval()
                 pred, trans_feat = classifier(simplified)
                 #tmp_logit = F.softmax(pred,dim=-1).max(-1)[0]
-                tmp_logit = pred.max(-1)[0]
-                logit.append(tmp_logit.reshape(-1,1))
 
-            sel = torch.gather(unselected_ind,dim=-1, index=torch.argmax(torch.cat(logit,dim=-1),dim=-1).reshape(-1,1))
+                cost_p2_p1 = square_distance(points.transpose(2, 1), simplified).min(-1)[0]
+                if args.max:
+                    cover_loss = -1 * args.beta * torch.mean(cost_p2_p1, dim=-1, keepdim=True)
+                else:
+                    cover_loss = -1* args.beta *torch.mean(cost_p2_p1,dim=-1,keepdim=True)
+
+                tmp_logit = pred.max(-1,keepdim=True)[0] + cover_loss
+                logit.append(tmp_logit)
+
+            sel = torch.gather(unselected_ind_batch,dim=-1, index=torch.argmax(torch.cat(logit,dim=-1),dim=-1,keepdim=True))
             #sel = torch.gather(unselected_ind, dim=-1, index=torch.LongTensor(2468, 1).random_(0, 1024-i).cuda())
-            selected_ind = torch.cat((selected_ind,sel),dim=-1)
-            unselected = torch.ones((2468, 1024),dtype=bool).cuda().scatter_(dim=-1,index=selected_ind,src=torch.tensor(0,dtype=bool))
 
-            simplified = batched_index_select(points, 1, selected_ind)
-            classifier = model.eval()
-            simplified = simplified.transpose(2, 1)
-            pred, trans_feat = classifier(simplified)
-            pred_choice = pred.data.max(1)[1]
-            for cat in np.unique(target.cpu()):
-                classacc = pred_choice[target==cat].eq(target[target==cat].long().data).cpu().sum()
-                class_acc[cat,0]+= classacc.item()/float(points[target==cat].size()[0])
-                class_acc[cat,1]+=1
-            correct = pred_choice.eq(target.long().data).cpu().sum()
-            class_acc[:,2] =  class_acc[:,0] / class_acc[:,1]
-            class_acc = np.mean(class_acc[:,2])
-            instance_acc = correct.item() / points.size()[0]
+            selected_ind = torch.cat((selected_ind_batch,sel),dim=-1)
+            unselected[ind_l: ind_h] = torch.ones((N, 1024),dtype=bool).cuda().scatter_(dim=-1,index=selected_ind,src=torch.tensor(0,dtype=bool))
+            selected[ind_l: ind_h] = torch.zeros((N, 1024),dtype=bool).cuda().scatter_(dim=-1,index=selected_ind,src=torch.tensor(1,dtype=bool))
 
-            print('%f  Test Instance Accuracy: %f, Class Accuracy: %f' % ( i, instance_acc, class_acc))
-            writer.add_scalar('acc/test_i', instance_acc,i )
-            writer.add_scalar('acc/test_c', class_acc, i)
+        selected_ind = index[selected].reshape(2468, -1)
+        p = torch.cat(p,dim=0)
+        t = torch.cat(t,dim=0)
+        simplified = batched_index_select(p, 1, selected_ind)
+        classifier = model.eval()
+        simplified = simplified.transpose(2, 1)
+        pred, trans_feat = classifier(simplified)
+        pred_choice = pred.data.max(1)[1]
+        for cat in np.unique(t.cpu()):
+            classacc = pred_choice[t==cat].eq(t[t ==cat].long().data).cpu().sum()
+            class_acc[cat,0]+= classacc.item()/float(p[t ==cat].size()[0])
+            class_acc[cat,1]+=1
+        correct = pred_choice.eq(t.long().data).cpu().sum()
+
+
+        class_acc[:,2] =  class_acc[:,0] / class_acc[:,1]
+        class_acc = np.mean(class_acc[:,2])
+        instance_acc = correct.item()/ p.size()[0]
+        print('%f  Test Instance Accuracy: %f, Class Accuracy: %f' % ( i, instance_acc, class_acc))
+        writer.add_scalar('acc/test_i', instance_acc,i )
+        writer.add_scalar('acc/test_c', class_acc, i)
 
 
 
@@ -176,7 +275,7 @@ def main(args):
 
     trainset, testset = get_datasets(args)
     # dataloader
-    testDataLoader = torch.utils.data.DataLoader(testset, batch_size=2468, shuffle=False, num_workers=4)
+    testDataLoader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=4)
 
     '''MODEL LOADING'''
     num_class = 40
@@ -195,7 +294,8 @@ def main(args):
 
 
     with torch.no_grad():
-        test_greedy(classifier.eval(), testDataLoader, writer)
+
+        test_greedy_batch(classifier.eval(), testDataLoader, writer)
     writer.close()
 
 if __name__ == '__main__':
